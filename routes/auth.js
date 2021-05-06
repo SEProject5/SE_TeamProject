1716ea250e838b824905d4695eb2eaeaa562bac8
uijin <uijin0531@gmail.com>
2021-05-06 16:01:23 +0900

    비밀번호 정규식 설정

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const User = require('../models/user');
 
+// 비밀번호 정규식 설정 (영문, 숫자, 특수문자 조합, 8~16자리)
+const chk_password = /^(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])(?=.*[0-9]).{8,16}$/;
+
 const router = express.Router();
 
 //회원가입 라우터
@@ -13,6 +16,11 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
     const exUser = await User.findOne({ where: { id } });
     if (exUser) { //id가 존재하면 /login으로 라우팅
       return res.redirect('/login?error=exist');
+    } else {
+      // 비밀번호 정규식 확인 
+      if (chk_password.test(password) === false) {
+          throw Error("비밀번호는 영문, 숫자, 특수문자 조합의 8~16자리여야 합니다."); 
+      }
     }
     const hash = await bcrypt.hash(password, 12); //비밀번호 암호화
     await User.create({ //사용자 정보 생성
