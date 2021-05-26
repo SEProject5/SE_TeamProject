bda81041ec87b6a468890fd6d02488389af69251
uijin <uijin0531@gmail.com>
2021-05-26 13:17:57 +0900

    no message

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { sequelize } = require('./models');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 sequelize.sync({ force: false }) //force가 True이면 서버 실행 시마다 테이블 재생성
   .then(() => {
     console.log('데이터베이스 연결 성공');
